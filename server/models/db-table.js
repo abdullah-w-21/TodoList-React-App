e11afd03c4e6c0dd71b290505c8f8f4a07bf9679
Abdullah Wasim <81172855+abdullah-w-21@users.noreply.github.com>
2023-09-04 05:49:57 +0500

    Add files via upload

---
+++ server/models/db-table.js
@@ -0,0 +1,25 @@
+// Create Database Model - Table Name and Column Name
+module.exports = (sequelize, DataTypes) => {
+  const todoTable = sequelize.define('todolists', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    jobtodo: {
+      type: DataTypes.STRING(4294967295),
+      allowNull: false
+    },
+    toggle: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+   
+     // Timestamps
+     createdAt: DataTypes.DATE,
+     updatedAt: DataTypes.DATE
+  })
+  return todoTable
+}
