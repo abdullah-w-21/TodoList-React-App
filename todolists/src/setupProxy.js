090db37d32b477a36af13df8b3a0fef6797b130e
Abdullah Wasim <81172855+abdullah-w-21@users.noreply.github.com>
2023-09-04 05:55:42 +0500

    Add files via upload

---
+++ todolists/src/setupProxy.js
@@ -0,0 +1,19 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
+module.exports = function (app) {
+    try{
+    app.use(
+        '/graphql',
+        createProxyMiddleware({
+            target: 'http://localhost:4000/',
+            changeOrigin: true,
+        })
+    );
+    }
+    catch(err){
+        console.log(err.message);
+    }
+};
+
+
