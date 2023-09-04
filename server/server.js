fee2d667203b4edf05921178ee9e894961e85d99
Abdullah Wasim <81172855+abdullah-w-21@users.noreply.github.com>
2023-09-04 05:48:04 +0500

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,23 @@
+// https://graphql.org/graphql-js/
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const dotenv = require('dotenv')
+const schema = require('./schema/index')
+const app = express()
+require('./models')
+// const PORT = 4000
+app.use(express.json({limit: "4096mb", extended: true}))  //{limit: "10mb", extended: true} Added to fix ERROR:request entity too large
+app.use(express.urlencoded({limit: "4096mb", extended: true, parameterLimit: 4294967295})) //app.use(express.urlencoded({limit: "10mb", extended: true, parameterLimit: 50000})) --->This line is all a part of That Error.
+dotenv.config({ path: '../config.env' })
+const PORT = process.env.PROXY_PORT || 5000
+
+app.use(
+  '/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+)
+
+app.listen(PORT, () => {
+  console.log(`SUCCESS: Running GraphQL server at http://localhost:${PORT}`)
+})
